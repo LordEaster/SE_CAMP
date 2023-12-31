5558606949d191f7c1eb4acb5aaf4bb1628094e0
Smart | LordEaster <smary1172@gmail.com>
2023-12-31 20:36:54 +0700

    update multiple table css

--- example-app/resources/views/multiple-table/multi-landing.blade.php
+++ example-app/resources/views/multiple-table/multi-landing.blade.php
@@ -9,12 +9,15 @@
     <title>Multiplication table | P. Natnawat</title>
 </head>
 <body>
+    <div class="multiple-space">
     <form method="POST" action="/multi-table">
+        <h1>Multiplication table</h1>
         <div style="display: flex; ">
             @csrf
             <input type="number" name="multiply" placeholder="Enter your multiply">
             <button type="submit">submit</button>
         </div>
     </form>
+    </div>
 </body>
 </html>
